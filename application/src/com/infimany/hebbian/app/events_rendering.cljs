ff4ef99fb75588ab6ec828435db7ef9f251f985c
Baber Khalil <Baber.s.Khalil@googlemail.com>
2014-02-28 17:11:26 +0000

    [BK] #Tech - work in progress.  Added geolocation to screen coords translation and some more visual effects.

--- application/src/com/infimany/hebbian/app/events_rendering.cljs
+++ application/src/com/infimany/hebbian/app/events_rendering.cljs
@@ -17,16 +17,30 @@
 )
 
 (def date-fmt "YYYY-MM-DDThh:mm:ssZ")
-(def origin (atom "geohash-origin"))
-(def scale 100)
+(def origin {:lat 51.734262 :lng -0.455852})
+(def scale 10000)
+(def width 1200)
+(def height 800)
+(def screen-origin {:x (/ width 2) :y (/ height 2)})
+(def event-x-translation 65)
+(def event-y-translation 80)
+
 
 ; positioning functions.
 
 (defn get-distance [{geohash :location}]
   3 )
 
+(defn scale-translate [point]
+  [(int (+ (:x screen-origin) (* scale (:x point)))) (+ (:y screen-origin) (* scale (:y point)))]
+)
+
+(defn move-event-to-center [point]
+  [(- (first point) event-x-translation) (- (last point) event-y-translation )]
+)
+
 (defn get-screen-loc [{geolocation :geolocation}]
-  [(* 5000 (:lng geolocation)) 0]
+  (move-event-to-center (scale-translate {:x (- (:lng geolocation) (:lng origin)) :y (- (:lat origin) (:lat geolocation))}))
 )
 
 (defn add-z-plane [events]
@@ -38,14 +52,15 @@
 
 
 (defn to-renderable [event]
-    (merge event {:distance (get-distance event)} {:location (get-screen-loc event)}))
+    (merge event {:distance (get-distance event)} {:screen-location (get-screen-loc event)}))
 
 (defn get-position-css [renderable-event]
-  (let [location (:location renderable-event)]
+  (let [location (:screen-location renderable-event)]
     {
      :position "absolute"
-     :left (str (first location) "px")
      :top (str (last location) "px")
+     :left (str (first location) "px")
+
      }
     )
 )
@@ -60,7 +75,6 @@
 
 (defn get-location-css [renderable-event]
   (clj->js (merge (get-position-css renderable-event)
-                  (get-dimension-css renderable-event)
                   (get-z-plan-css renderable-event) ) )
 )
 
@@ -79,7 +93,7 @@
         (fn []
           (this-as this
                    (let [event-data (js->clj (.. this -props -event) :keywordize-keys true)]
-                     (div #js {:className "event" :style  (get-location-css event-data :keywordize-keys true)}
+                     (div #js {:className "event" :style  (get-location-css event-data)}
                           (div #js {:className "distance"} (:distance event-data) )
                           (div #js {:className "details"} (:details event-data))
                           (let [start-time (:start-time event-data) end-time (:end-time event-data)]
@@ -92,7 +106,6 @@
    )
   )
 
-
 (def EventUniverse
   (js/React.createClass
    #js {
@@ -101,10 +114,15 @@
 
         :render
         (fn []
-          (this-as this (div #js {:style #js {:width "1200px" :height "800px" :overflow-x "scroll"}
+          (this-as this (div #js {:style #js {:width (str width "px") :height (str height "px")}
                                   :onWheel (.. this -handleMouseWheel)
                                   }
-
+                             (into-array (for [marker @origin-markers]
+                                           (div (clj->js {:className "tunnel" :style
+                                                          {:position "absolute"
+                                                           :left (str (first (:screen-location marker))"px")
+                                                           :top (str (last (:screen-location marker))"px")
+                                                           :-webkit-transform (str "translateZ(" (:z-plane marker) "px)")}}))))
                              (into-array (map #(Event #js {:event %}) (.. this -state -events)) )
                              ))
           )
@@ -132,7 +150,12 @@
 (def pan-channel (async/chan (async/sliding-buffer 100)))
 (def zoom-channel (async/chan (async/sliding-buffer 100)))
 
+(defn generate-origin-markers []
+  (for [z-plane (range 0 -2000 -200)] {:z-plane z-plane :screen-location [(:x screen-origin) (:y screen-origin)]})
+)
+
 (def events (atom []))
+(def origin-markers (atom (generate-origin-markers)) )
 
 
 
@@ -148,13 +171,11 @@
 
 (services/get-events events-channel)
 (go (let [raw-events (async/<! events-channel)]
-      (.log js/console "Here I am!!!!")
       (swap! events #(map to-renderable (add-z-plane (map convert-times %2))) raw-events )
       (.setState event-universe #js {:events (clj->js @events)})
       ))
 
 
-@events
 
 ; end React components.
 
@@ -163,7 +184,7 @@
 )
 
 (defn shift-locations [events delta]
-  (map #(update-in % [:location] shift-location delta) events)
+  (map #(update-in % [:screen-location] shift-location delta) events)
   )
 
 
@@ -172,7 +193,7 @@
 )
 
 
-(defn shift-time-planes [events delta]
+(defn shift-z-planes [events delta]
   (map #(update-in % [:z-plane] shift-z-plane delta) events)
 )
 
@@ -180,11 +201,11 @@
 (go (while true
         (let [[value channel] (alts! [pan-channel zoom-channel])]
           (cond
-           (= pan-channel channel) (swap! events shift-locations value)
-           (= zoom-channel channel) (swap! events shift-time-planes value)
+           (= pan-channel channel) (do (swap! events shift-locations value) (swap! origin-markers shift-locations value) )
+           (= zoom-channel channel) (do (swap! events shift-z-planes value) (swap! origin-markers shift-z-planes value))
            ) )
-      (.setState event-universe #js {:events (clj->js @events)})
-))
+      (.setState event-universe #js {:events (clj->js @events) :origin-markers (clj->js @origin-markers)})
+      ))
 
 
 
