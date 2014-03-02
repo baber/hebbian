c647193442f757c46a8e6143553f38c30ca5984c
Baber Khalil <Baber.s.Khalil@googlemail.com>
2014-03-02 16:47:58 +0000

    [BK] #Tech - moved functions around into new homes to partition better by namespace.

--- application/src/com/infimany/hebbian/app/events_rendering.cljs
+++ application/src/com/infimany/hebbian/app/events_rendering.cljs
@@ -3,6 +3,7 @@
    [dommy.core :as dommy]
    [cljs.core.async :as async]
    [com.infimany.hebbian.app.services :as services]
+   [com.infimany.hebbian.app.ui-components.events :as event-ui]
 
    )
 
@@ -76,90 +77,20 @@
 ; React components.
 
 
-(def Event
-  (js/React.createClass
-   #js {
 
-        :render
-        (fn []
-          (this-as this
-                   (let [event-data (js->clj (.. this -props -event) :keywordize-keys true)]
-                     (div #js {:className "event" :style  (get-location-css event-data)}
-                          (div #js {:className "distance"} (:distance event-data) )
-                          (div #js {:className "details"} (:details event-data))
-                          (let [start-time (:start-time event-data) end-time (:end-time event-data)]
-                            (div #js {:className "time"} (.format (:start-time event-data) "DD MMM YYYY")))
-                          )))
-
-          )
-
-        }
-   )
-  )
-
-(def EventUniverse
-  (js/React.createClass
-   #js {
-
-        :getInitialState (fn [] (clj->js {:events @events}))
-
-        :render
-        (fn []
-          (this-as this (div #js {:style #js {:width (str width "px") :height (str height "px")}
-                                  :onWheel (.. this -handleMouseWheel)
-                                  }
-                             (into-array (for [marker @origin-markers]
-                                           (div (clj->js {:className "tunnel" :style
-                                                          {:position "absolute"
-                                                           :left (str (first (:screen-location marker))"px")
-                                                           :top (str (last (:screen-location marker))"px")
-                                                           :-webkit-transform (str "translateZ(" (:z-plane marker) "px)")}}))))
-                             (into-array (map #(Event #js {:event %}) (.. this -state -events)) )
-                             ))
-          )
-
-        :handleMouseWheel
-        (fn [event]
-          (.preventDefault event)
-          (let [delta-x (.. event -deltaX) delta-y (.. event -deltaY)]
-            (go
-             (cond
-              (>= (js/Math.abs delta-y) (js/Math.abs delta-x)) (>! zoom-channel {:x delta-x :y delta-y})
-              :else (>! pan-channel {:x delta-x :y delta-y}))
-             )
-            )
-          )
-
-        }
-   )
-
-)
-
-; channels.
-
-(def events-channel (async/chan))
-(def pan-channel (async/chan (async/sliding-buffer 100)))
-(def zoom-channel (async/chan (async/sliding-buffer 100)))
 
 (defn generate-origin-markers []
   (for [z-plane (range 0 -2000 -200)] {:z-plane z-plane :screen-location [(:x screen-origin) (:y screen-origin)]})
 )
 
 (def events (atom []))
-(def origin-markers (atom (generate-origin-markers)) )
+(def markers (atom (generate-origin-markers)) )
 
 
-(def event-universe (EventUniverse))
-
-(js/React.renderComponent
- event-universe
- (.getElementById js/document "events"))
-
-
-(services/get-events events-channel)
-(go (let [raw-events (async/<! events-channel)]
+(services/get-events event-ui/events-channel)
+(go (let [raw-events (async/<! event-ui/events-channel)]
       (swap! events #(map to-renderable (add-z-plane (map convert-times %2))) raw-events )
-      (.setState event-universe #js {:events (clj->js @events)})
+      (.setState event-ui/event-universe #js {:events (clj->js @events) :markers (clj->js @markers)})
       ))
 
 
@@ -186,12 +117,13 @@
 
 ; kick off event loop.
 (go (while true
-        (let [[value channel] (alts! [pan-channel zoom-channel])]
+        (let [[value channel] (async/alts! [event-ui/pan-channel event-ui/zoom-channel])]
           (cond
-           (= pan-channel channel) (do (swap! events shift-locations value) (swap! origin-markers shift-locations value) )
-           (= zoom-channel channel) (do (swap! events shift-z-planes value) (swap! origin-markers shift-z-planes value))
+           (= event-ui/pan-channel channel) (do (swap! events shift-locations value) (swap! markers shift-locations value) )
+           (= event-ui/zoom-channel channel) (do (swap! events shift-z-planes value) (swap! markers shift-z-planes value))
            ) )
-      (.setState event-universe #js {:events (clj->js @events) :origin-markers (clj->js @origin-markers)})
+      (.log js/console "setting state!!")
+      (.setState event-ui/event-universe #js {:events (clj->js @events) :markers (clj->js @markers)})
       ))
 
 
---
+++ application/src/com/infimany/hebbian/app/ui_components/events.cljs
@@ -0,0 +1,110 @@
+(ns com.infimany.hebbian.app.ui-components.events
+  (:require
+   [cljs.core.async :as async]
+   )
+
+  (:use
+   [React.DOM :only [div]]
+   )
+
+  (:require-macros
+   [cljs.core.async.macros :refer [go]])
+
+  )
+
+(def width 1200)
+(def height 800)
+
+
+(defn get-position-css [{location :screen-location z-plane :z-plane}]
+  {:position "absolute"
+   :-webkit-transform (str "translate3d(" (first location) "px," (last location) "px," z-plane "px)")
+   }
+  )
+
+(defn get-location-css [renderable-event]
+  (clj->js (get-position-css renderable-event))
+)
+
+
+(def events-channel (async/chan))
+(def pan-channel (async/chan (async/sliding-buffer 100)))
+(def zoom-channel (async/chan (async/sliding-buffer 100)))
+
+
+(def OriginMarker
+  (js/React.createClass
+   #js {
+        :render
+        (fn [] (this-as this
+                              (let [marker (js->clj (.. this -props -marker) :keywordize-keys true)]
+                                (div #js {:className "tunnel" :style  (get-location-css marker)})
+                                )
+
+                              ))
+        }
+   )
+  )
+
+
+(def Event
+  (js/React.createClass
+   #js {
+
+        :render
+        (fn []
+          (this-as this
+                   (let [event (js->clj (.. this -props -event) :keywordize-keys true)]
+                     (div #js {:className "event" :style  (get-location-css event)}
+                          (div #js {:className "distance"} (:distance event) )
+                          (div #js {:className "details"} (:details event))
+                          (let [start-time (:start-time event) end-time (:end-time event)]
+                            (div #js {:className "time"} (.format (:start-time event) "DD MMM YYYY")))
+                          )))
+
+          )
+
+        }
+   )
+  )
+
+(def EventUniverse
+  (js/React.createClass
+   #js {
+
+        :getInitialState (fn [] (clj->js {:events [] :markers []}))
+
+        :render
+        (fn []
+          (this-as this (div #js {:style #js {:width (str width "px") :height (str height "px")}
+                                  :onWheel (.. this -handleMouseWheel)
+                                  }
+                             (into-array (map #(OriginMarker #js {:marker %}) (.. this -state -markers)) )
+                             (into-array (map #(Event #js {:event %}) (.. this -state -events)) )
+                             ))
+          )
+
+        :handleMouseWheel
+        (fn [event]
+          (.preventDefault event)
+          (let [delta-x (.. event -deltaX) delta-y (.. event -deltaY)]
+            (go
+             (cond
+              (>= (js/Math.abs delta-y) (js/Math.abs delta-x)) (async/>! zoom-channel {:x delta-x :y delta-y})
+              :else (async/>! pan-channel {:x delta-x :y delta-y}))
+             )
+            )
+          )
+
+        }
+   )
+
+)
+
+
+(def event-universe (EventUniverse))
+
+(js/React.renderComponent
+ event-universe
+ (.getElementById js/document "events"))
+
